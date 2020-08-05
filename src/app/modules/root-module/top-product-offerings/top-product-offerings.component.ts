43f3e73f21d8560ebf79d17aa9334dc0fb92d2d2
desmond <desmond1999d@mail.ru>
2020-08-05 12:31:58 +0300

    Add mobile offerings carousel

--- src/app/modules/root-module/top-product-offerings/top-product-offerings.component.ts
+++ src/app/modules/root-module/top-product-offerings/top-product-offerings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ProductType} from '../../../shared/ProductType';
 import {ProductTypeService} from '../../../services/ProductTypeService';
+import { tns } from "../../../../../node_modules/tiny-slider/src/tiny-slider"
 
 @Component({
   selector: 'app-top-product-offerings',
@@ -15,6 +16,34 @@ export class TopProductOfferingsComponent implements OnInit {
 
   ngOnInit() {
     this.productTypeHierarchy = this.productTypeService.getServiceHierarchy();
+  }
+
+  ngAfterViewInit() {
+    tns({
+      container: '.m-products-slider',
+      items: 1,
+      mouseDrag: true,
+      center: true,
+      lazyload: true,
+      autoplay: true,
+      autoplayButton: false,
+      autoplayHoverPause: true,
+      autoplayButtonOutput: false,
+      controls: false,
+      nav: false,
+      responsive: {
+        0: {
+          edgePadding: 40,
+          gutter: 30,
+          items: 1
+        },
+        640: {
+          edgePadding: 40,
+          gutter: 30,
+          items: 2
+        }
+      }
+    });
   }
 
 }
