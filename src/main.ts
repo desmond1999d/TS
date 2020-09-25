a3176c39e0e6d8380111f984267a070be607c334
desmond <desmond1999d@mail.ru>
2020-09-26 00:25:49 +0300

    build fix and add todo

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,9 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+// TODO: Наши услуги - баг при прокручивании. Возможно и скорее всего происходит из-за строчки "skipLibCheck": true в файле tsconfig.json
+// TODO: we are cool перерисовать иконки и сделать их png
+// TODO: Наши работы сделаны по паддингам снизу как-то криво
+// TODO: Добавить разные иконки контактов
+// TODO: Приложите файл в контактах скрепочка кривая
