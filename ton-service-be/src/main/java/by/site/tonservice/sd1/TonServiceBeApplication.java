de3856b33c6da04df47d73586f0509f8e02da802
desmond <desmond1999d@mail.ru>
2020-12-17 17:13:58 +0300

    Fix schema_creation and add data and schema sql files

--- ton-service-be/src/main/java/by/site/tonservice/sd1/TonServiceBeApplication.java
+++ ton-service-be/src/main/java/by/site/tonservice/sd1/TonServiceBeApplication.java
@@ -1,11 +1,14 @@
 package by.site.tonservice.sd1;
 
+import by.site.tonservice.sd1.service.DbInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TonServiceBeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TonServiceBeApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(TonServiceBeApplication.class, args);
+        context.getBean(DbInitializer.class).initProductExamples();
     }
 }
