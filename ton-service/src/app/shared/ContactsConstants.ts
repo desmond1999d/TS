e1b397d70eca89c63da7bfc46f26079abe546ddc
desmond <desmond1999d@mail.ru>
2023-01-21 14:43:36 +0300

    change phone number

--- ton-service/src/app/shared/ContactsConstants.ts
+++ ton-service/src/app/shared/ContactsConstants.ts
@@ -5,6 +5,6 @@ export class ContactsConstants {
   public readonly TIMETABLE = 'ПН-ПТ 09:00-17:00';
   public readonly ADDRESS = 'СОДО «Тон-сервис» г.Гродно, ул. Октябрьская 4';
   public readonly EMAIL = 'innagrodno@mail.ru';
-  public readonly PHONE = '(0152) 74-00-90, (029) 78-000-23';
+  public readonly PHONE = '(0152) 68-03-90, (029) 78-000-23';
   public readonly VIBER = '(033) 361-00-11';
 }
